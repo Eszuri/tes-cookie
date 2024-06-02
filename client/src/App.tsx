915045167aee2cb68c71427e5fa5eac2fc89195a
ElynaRia <elynaria28@gmail.com>
2024-06-03 06:27:51 +0700

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,40 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
   const setCookie = () => {
-    axios.post('http://localhost:4000/tes', { text: "This Is Text" }, { withCredentials: true })
-      .then(res => {
-        alert(res)
+    axios
+      .post(
+        "http://localhost:4000/set",
+        { text: "This Is Text" },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        alert(JSON.stringify(res));
       })
-  }
+      .catch((err) => {
+        alert("Error is:" + err);
+      });
+  };
 
   return (
     <>
-      <div><a href="https://vitejs.dev" target="_blank"><img src={viteLogo} className="logo" alt="Vite logo" /></a><a href="https://react.dev" target="_blank"><img src={reactLogo} className="logo react" alt="React logo" /></a></div><h1>Vite + React</h1><div className="card">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
         <button onClick={setCookie}>set Cookie</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,23 +1,32 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send("HOME");
+app.get("/", (req, res) => {
+  res.send("HOME");
 });
 
-app.post('/set', (req, res) => {
-    const text = req.body.text;
-    res.cookie('/the_text', text, {
-        expires: 999999,
-        maxAge: 999999,
-        secure: true
-    }).send('success');
-})
-
+app.post("/set", (req, res) => {
+  const text = req.body.text;
+  res
+    .cookie("/the_text", text, {
+      expires: 999999,
+      maxAge: 999999,
+      secure: true,
+    })
+    .send("success");
+});
 
 app.listen(4000, () => {
-    console.log('running');
-})
+  console.log("running");
+});
